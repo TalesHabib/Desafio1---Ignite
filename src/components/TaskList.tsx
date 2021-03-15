5ebf7348f66a22a5f37648d2fe93c5d74dce4f41
Tales Habib <tales.habib@gmail.com>
2021-03-15 18:06:16 -0300

    desafio finalizado

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -12,18 +12,37 @@ interface Task {
 
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
-  const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [newTaskTitle, setNewTaskTitle] = useState('Atividade física');
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return console.log('does not permitted!');
+
+    const newTask = {
+      id: Math.random() * 100,
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+      setTasks(oldTask => [...oldTask, newTask]);
+      setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete,
+    } : task)
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const FilteredTasks = tasks.filter(
+      task => task.id !== id
+    );
+    setTasks(FilteredTasks)
   }
 
   return (
